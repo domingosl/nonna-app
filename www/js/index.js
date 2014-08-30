e63885a58ea908c3eacded901cb447f1d0eb3ea2
domingosl <ing.domingo.sl@gmail.com>
2014-08-30 17:01:06 +0200

    no comment

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,19 @@
+$('#take-photo-button').click(function() {
+    capturePhoto();
+});
+
+function renderUI() {
+
+}
+
+
 var pictureSource;   // picture source
 var destinationType; // sets the format of returned value
 
 function onDeviceReady() {
     pictureSource = navigator.camera.PictureSourceType;
     destinationType = navigator.camera.DestinationType;
-    capturePhoto();
+    renderUI();
 }
 
 function clearCache() {
@@ -54,36 +63,15 @@ function onFail(message) {
 
 
 var app = {
-
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        $('#test-div').html('HELLO WORLD!');
         onDeviceReady();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
